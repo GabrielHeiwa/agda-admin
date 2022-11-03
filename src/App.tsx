783134a20cd50c256dbb694b3bfe67bcbae2f3cb
gabrielheiwa <heiwa.gabriel@gmail.com>
2022-11-03 11:50:57 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import buildHasuraProvider from "ra-data-hasura";
+import { Admin, Resource, DataProvider, ListGuesser, EditGuesser, ShowGuesser } from "react-admin";
+import { InMemoryCache, ApolloClient } from "@apollo/client";
 
-function App() {
+const apollo = new ApolloClient({
+  uri: "http://localhost:8080/v1/graphql",
+
+  headers: {
+    "x-hasura-admin-secret": "hermes123",
+
+    // "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUhhc3VyYS1Vc2VyLUlkIjoiMSIsIlgtSGFzdXJhLVJvbGUiOiJ1c2VyIiwiaWF0IjoxNTE2MjM5MDIyfQ.C64IooXjOB7H32ta9UoBt_R7BVzdd_nj_4pT7AyJtoI"
+  },
+  cache: new InMemoryCache(),
+});
+
+const App = () => {
+  const [dataProvider, setDataProvider] = useState<DataProvider>();
+
+  useEffect(() => {
+    const buildDataProvider = async () => {
+      const dataProvider = await buildHasuraProvider({
+        client: apollo,
+      });
+      setDataProvider(() => dataProvider);
+    };
+    buildDataProvider();
+  }, []);
+
+  if (!dataProvider) return <p>Loading...</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Admin dataProvider={dataProvider}>
+      <Resource
+        name="users"
+        list={ListGuesser}
+        edit={EditGuesser}
+        create={ShowGuesser}
+      />
+
+      <Resource
+        name="phones"
+        list={ListGuesser}
+        // edit={PostEdit}
+        // create={PostCreate}
+      />
+
+      <Resource
+        name="phone_numbers"
+        list={ListGuesser}
+        // edit={PostEdit}
+        // create={PostCreate}
+      />
+      
+      <Resource
+        name="messages"
+        list={ListGuesser}
+        // edit={PostEdit}
+        // create={PostCreate}
+      />
+      
+      <Resource
+        name="messages_groups"
+        list={ListGuesser}
+        // edit={PostEdit}
+        // create={PostCreate}
+      />
+
+      <Resource
+        name="phone_numbers_group"
+        list={ListGuesser}
+        // edit={PostEdit}
+        // create={PostCreate}
+      />
+
+    </Admin>
   );
-}
+};
 
 export default App;
